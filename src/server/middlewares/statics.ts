9ff4a072764d205512952cbfe859deb91f468596
Rory Pearson <52637194+Mezmerizxd@users.noreply.github.com>
2022-11-06 01:04:24 +0000

    Update statics.ts

--- src/server/middlewares/statics.ts
+++ src/server/middlewares/statics.ts
@@ -11,7 +11,7 @@ export default new (class Statics {
         app.use(
             express.static(path.join(__dirname, `../../../release/client/`))
         );
-        app.get('/', (req, res) => {
+        app.get('/*', (req, res) => {
             res.sendFile(
                 path.join(__dirname, `../../../release/client/index.html`)
             );
