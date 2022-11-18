2c459b23b0944404b342b2ed238907b1af33cf19
Mezmerizxd <rorypearson2003@hotmail.com>
2022-11-18 22:08:32 +0000

    [server]handleFriendRequest, accept & decline

--- src/server/api/v1/user/handleFriendRequest.ts
+++ src/server/api/v1/user/handleFriendRequest.ts
@@ -2,6 +2,8 @@ import { Request, Response } from 'express';
 import Responder from '../responder';
 import Log from '../../../utils/Log';
 import Features from '../features';
+import Firebase from '../../../data/firebase';
+import Cfg from '../../../cfg';
 
 type RequestBody = {
     userId: number;
@@ -29,13 +31,6 @@ export default new (class HandleFriendRequest {
                 Responder(res, 'error', null, 'A type is required.');
                 return;
             }
-            if (
-                body.type.toUpperCase() !== 'ACCEPT' ||
-                body.type.toUpperCase() !== 'DECLINE'
-            ) {
-                Responder(res, 'error', null, 'Invalid type.');
-                return;
-            }
 
             const userData: any = await Features.getUserData(
                 'authorization',
@@ -51,12 +46,166 @@ export default new (class HandleFriendRequest {
                 return;
             }
 
-            switch (body.type) {
+            const userFriends: any = [];
+            const userRequestsReceived: any = [];
+            const userRequestsSent: any = [];
+            if (userData?.friends) {
+                Object.keys(userData.friends).forEach(async (friend) => {
+                    userFriends.push(userData.friends[friend]);
+                });
+                Object.keys(userData.friendRequests?.received).forEach(
+                    async (request) => {
+                        userRequestsReceived.push(
+                            userData.friendRequests?.received[request]
+                        );
+                    }
+                );
+                Object.keys(userData.friendRequests?.sent).forEach(
+                    async (request) => {
+                        userRequestsSent.push(
+                            userData.friendRequests?.sent[request]
+                        );
+                    }
+                );
+            }
+
+            const friendFriends: any = [];
+            const friendRequestsSent: any = [];
+            const friendRequestsReceived: any = [];
+            if (friendData?.friends) {
+                Object.keys(friendData.friends).forEach(async (friend) => {
+                    friendFriends.push(friendData.friends[friend]);
+                });
+                Object.keys(friendData.friendRequests?.sent).forEach(
+                    async (request) => {
+                        friendRequestsSent.push(
+                            friendData.friendRequests?.sent[request]
+                        );
+                    }
+                );
+                Object.keys(friendData.friendRequests?.received).forEach(
+                    async (request) => {
+                        friendRequestsReceived.push(
+                            friendData.friendRequests?.received[request]
+                        );
+                    }
+                );
+            }
+
+            switch (body.type.toUpperCase()) {
                 case 'ACCEPT':
+                    // if user does not have userId in friends continue
+                    if (userFriends.includes(friendData.userId)) {
+                        Responder(
+                            res,
+                            'error',
+                            null,
+                            'You are already friends.'
+                        );
+                        return;
+                    }
+                    // if friend does not have user userId in friends continue
+                    if (friendFriends.includes(userData.userId)) {
+                        Responder(
+                            res,
+                            'error',
+                            null,
+                            'You are already friends.'
+                        );
+                        return;
+                    }
+
+                    userFriends.push(friendData.userId);
+                    friendFriends.push(userData.userId);
+
+                    // Remove from received
+                    await Firebase.database
+                        .ref(
+                            `${Cfg.Local().fbDbName}/${
+                                Cfg.Local().fbDbUserData
+                            }/${userData.userId}/friendRequests/received`
+                        )
+                        .set(
+                            userRequestsReceived.filter(
+                                (request) => request === friendData.userId
+                            )
+                        );
+                    // Remove from sent
+                    await Firebase.database
+                        .ref(
+                            `${Cfg.Local().fbDbName}/${
+                                Cfg.Local().fbDbUserData
+                            }/${friendData.userId}/friendRequests/sent`
+                        )
+                        .set(
+                            friendRequestsSent.filter(
+                                (request) => request === userData.userId
+                            )
+                        );
+                    // Add friend to user
+                    await Firebase.database
+                        .ref(
+                            `${Cfg.Local().fbDbName}/${
+                                Cfg.Local().fbDbUserData
+                            }/${userData.userId}/friends`
+                        )
+                        .set(userFriends);
+                    await Firebase.database
+                        .ref(
+                            `${Cfg.Local().fbDbName}/${
+                                Cfg.Local().fbDbUserData
+                            }/${friendData.userId}/friends`
+                        )
+                        .set(friendFriends);
                     break;
 
                 case 'DECLINE':
+                    friendRequestsReceived.filter(
+                        (request) => request === userData.userId
+                    );
+                    friendRequestsSent.filter(
+                        (request) => request === userData.userId
+                    );
+                    userRequestsSent.filter(
+                        (request) => request === friendData.userId
+                    );
+                    userRequestsReceived.filter(
+                        (request) => request === friendData.userId
+                    );
+                    await Firebase.database
+                        .ref(
+                            `${Cfg.Local().fbDbName}/${
+                                Cfg.Local().fbDbUserData
+                            }/${friendData.userId}/friendRequests/sent`
+                        )
+                        .set(friendRequestsSent);
+                    await Firebase.database
+                        .ref(
+                            `${Cfg.Local().fbDbName}/${
+                                Cfg.Local().fbDbUserData
+                            }/${friendData.userId}/friendRequests/received`
+                        )
+                        .set(friendRequestsReceived);
+
+                    await Firebase.database
+                        .ref(
+                            `${Cfg.Local().fbDbName}/${
+                                Cfg.Local().fbDbUserData
+                            }/${userData.userId}/friendRequests/sent`
+                        )
+                        .set(userRequestsSent);
+                    await Firebase.database
+                        .ref(
+                            `${Cfg.Local().fbDbName}/${
+                                Cfg.Local().fbDbUserData
+                            }/${userData.userId}/friendRequests/received`
+                        )
+                        .set(userRequestsReceived);
                     break;
+
+                default:
+                    Responder(res, 'error', null, 'Invalid type.');
+                    return;
             }
 
             // Return data
