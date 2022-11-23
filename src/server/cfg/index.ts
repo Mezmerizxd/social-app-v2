4e5e27d8ce19781903ee5353bdafc2d0b37e9864
Mezmerizxd <rorypearson2003@hotmail.com>
2022-11-23 20:24:30 +0000

    ci/cd test

--- src/server/cfg/index.ts
+++ src/server/cfg/index.ts
@@ -51,9 +51,7 @@ type UserApiReturn = {
 
 export default new (class Cfg {
     public Env = (): EnvReturn => {
-        if (process.env.PORT === null) {
-            dotenv.config({ path: path.join(__dirname, '../../../.env') });
-        }
+        dotenv.config({ path: path.join(__dirname, '../../../.env') });
         return {
             port: process.env.PORT,
             socketPort: process.env.SOCKET_PORT,
